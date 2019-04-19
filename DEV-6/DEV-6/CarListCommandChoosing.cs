7c3db14a7ccd58bf3a07aa7305d14d8261193af5
BabeyKirill <kotonycta@gmail.com>
2019-04-19 23:02:52 +0300

    DEV-6.commit-1

---
+++ DEV-6/DEV-6/CarListCommandChoosing.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DEV_6
+{
+    /// <summary>
+    /// Concrete command for car list
+    /// </summary>
+    class CarListCommandChoosing : ICommand
+    {
+        CarList carlist;
+
+        public CarListCommandChoosing(CarList carlist)
+        {
+            this.carlist = carlist;
+        }
+
+        /// <summary>
+        /// Calls the command specified in the console
+        /// </summary>
+        public void Execute()
+        {
+            while (true)
+            {
+                Console.Write("Write command: ");
+                string CommandName = Console.ReadLine();
+
+                if (CommandName == "count types")
+                {
+                    Console.WriteLine($"Number of brands is {carlist.GetCountTypes()}");
+                }
+                else if (CommandName == "count all")
+                {
+                    Console.WriteLine($"Total number of cars is {carlist.GetCountAll()}");
+                }
+                else if (CommandName == "average price")
+                {
+                    Console.WriteLine($"Average price of all cars is {carlist.GetAveragePrice()}");
+                }
+                else if (CommandName.IndexOf("average price") == 0 && CommandName.Length > ("average price ").Length)
+                {
+                    Console.WriteLine($"Average price of cars of brand is {carlist.GetAveragePriceType(CommandName)}");
+                }
+                else if (CommandName == "exit")
+                {
+                    Console.WriteLine("Programm was completed by your command");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Unknown Command");
+                }
+            }
+        }
+    }
+}
---
+++ DEV-6/DEV-6/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DEV_6
+{
+    //This program works with the list of cars 
+
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Invoker invoker = new Invoker();
+
+            try
+            {
+                CarList carlist = new CarList(args[0]);
+                invoker.SetCommand(new CarListCommandChoosing(carlist));
+                invoker.ExecuteCommand();
+            }
+            catch when (args.Length == 0)
+            {
+                Console.WriteLine("Command line parameter is not set.");
+            }
+            catch
+            {
+                Console.WriteLine("Unexpected error");
+            }
+        }       
+    }
+}

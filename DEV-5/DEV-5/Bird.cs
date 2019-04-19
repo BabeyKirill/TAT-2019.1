9b2cc5ec82a148aeca77c3360808241c8638e3af
BabeyKirill <kotonycta@gmail.com>
2019-04-19 22:57:30 +0300

    DEV-5.commit-1

---
+++ DEV-5/DEV-5/Bird.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_5
+{
+    class Bird : IFlyable
+    {
+        protected int coordinate_x = 0;
+        protected int coordinate_y = 0;
+        protected int coordinate_z = 0;
+
+        public void FlyTo(Point newPoint)
+        {
+            coordinate_x = newPoint.coordinate_x;
+            coordinate_y = newPoint.coordinate_y;
+            coordinate_z = newPoint.coordinate_z;
+        }
+
+        public double GetFlyTime()
+        {
+            int finish_coordinate_x = 100;
+            int finish_coordinate_y = 200;
+            int finish_coordinate_z = 800;
+            double flight_time = 0;
+            double distance = Math.Sqrt((finish_coordinate_x - coordinate_x) ^ 2 + (finish_coordinate_y - coordinate_y) ^ 2 + (finish_coordinate_z - coordinate_z) ^ 2);
+            Random random = new Random((DateTime.Now.Millisecond));
+            double speed = random.Next(0, 20);
+            flight_time = distance / speed;
+            return flight_time*3600;
+        }
+
+        public string WhoAmI()
+        {
+            return "I'm a bird";
+        }
+    }
+}
+
---
+++ DEV-5/DEV-5/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_5
+{
+    public struct Point
+    {
+        public int coordinate_x;
+        public int coordinate_y;
+        public int coordinate_z; 
+
+        public Point(int x, int y, int z)
+        {
+            this.coordinate_x = x;
+            this.coordinate_y = y;
+            this.coordinate_z = z;
+        }
+    }
+}
